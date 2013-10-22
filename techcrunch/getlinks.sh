1664c3a2d64bac159d4f75821c0f1005e4e90008
alexeyev <anton.m.alexeyev@gmail.com>
2013-10-23 02:01:38 +0400

    Minor code improvements + links update.

--- techcrunch/getlinks.sh
+++ techcrunch/getlinks.sh
@@ -1,35 +1,29 @@
 #!/bin/bash
-# code is dirty and hacky
-# but this is one-timer, isn't it?
+# Code is dirty and hacky, but this is one-timer, isn't it?
 
+# checking if page exists by given URL
 function is_blank {
 	res=$(curl $1 | grep -c '<title>Page not found')
-	#echo "$res page-not-found matches found!\n\n\n"
 	if [ $res -eq 1 ]
 		then echo "yep" 
 		else echo "nope" 
 	fi	
 }
 
-
-#if [[ $(is_blank $1) == 'yep' ]]
-#	then echo 'Pustota'
-#elif [[ $(is_blank $1) == 'nope' ]]
-#	then echo 'Chapayev'
-#else echo 'Clay machine gun'
-#fi
-
 current_date=$(date)
+
 > links.txt
 
 while :
 do
+	# creating URL slug '/{yyyy}/{MM}/{dd}/page/{n}/'
 	formatted=$(date -d "$current_date" +%Y/%m/%d)
 	current_date=$(date +%Y/%m/%d  -d "$formatted 1 days ago")
 	page=1
 	url="http://techcrunch.com/$current_date/page/$page/"
 	while [[ $(is_blank $url) == 'nope' ]]
 	do
+		# plain regex for extracting URLs of interest
 		curl $url | grep -o 'href=[^<>]*data-omni-sm="gbl_river_headline"' | sed -r 's/.*(http[^"]*)".*/\1/g' >> links.txt
 		echo "DONE WITH: $current_date, page $page, url: $url"
 		page=$(($page+1))
@@ -37,4 +31,3 @@ do
 		echo "TRYING: $current_date, page $page, url: $url"
 	done
 done
-
