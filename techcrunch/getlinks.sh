f492fee06105effcc7c9627f458e93b136a611ea
alexeyev <anton.m.alexeyev@gmail.com>
2013-10-23 01:52:44 +0400

    Link collector seems to be more or less valid now.

--- techcrunch/getlinks.sh
+++ techcrunch/getlinks.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+# code is dirty and hacky
+# but this is one-timer, isn't it?
 
 function is_blank {
 	res=$(curl $1 | grep -c '<title>Page not found')
@@ -18,19 +20,20 @@ function is_blank {
 #fi
 
 current_date=$(date)
+> links.txt
 
 while :
 do
 	formatted=$(date -d "$current_date" +%Y/%m/%d)
 	current_date=$(date +%Y/%m/%d  -d "$formatted 1 days ago")
 	page=1
-	url="http://techcrunch.com/$current_date/page/$page"
+	url="http://techcrunch.com/$current_date/page/$page/"
 	while [[ $(is_blank $url) == 'nope' ]]
-	do 
-		wget -q $url -P raw_links
+	do
+		curl $url | grep -o 'href=[^<>]*data-omni-sm="gbl_river_headline"' | sed -r 's/.*(http[^"]*)".*/\1/g' >> links.txt
 		echo "DONE WITH: $current_date, page $page, url: $url"
 		page=$(($page+1))
-		url="http://techcrunch.com/$current_date/page/$page"
+		url="http://techcrunch.com/$current_date/page/$page/"
 		echo "TRYING: $current_date, page $page, url: $url"
 	done
 done
