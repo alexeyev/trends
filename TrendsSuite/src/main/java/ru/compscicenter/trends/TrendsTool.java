5ad7471223b940b8260f25c25e716e3950ba7a12
alexeyev <anton.m.alexeyev@gmail.com>
2013-11-24 15:08:26 +0400

    Trends extractor works.

--- TrendsSuite/src/main/java/ru/compscicenter/trends/TrendsTool.java
+++ TrendsSuite/src/main/java/ru/compscicenter/trends/TrendsTool.java
@@ -1,6 +1,6 @@
 package ru.compscicenter.trends;
 
-import com.hp.hpl.taxonomy.Extract;
+import com.hp.hpl.taxonomy.TaxonomyExtractor;
 import com.hp.hpl.taxonomy.TaxonomyExtractorBasic;
 import com.hp.hpl.taxonomy.Term;
 import com.hp.hpl.taxonomy.Topic;
@@ -16,19 +16,31 @@ public class TrendsTool {
 
     private static Logger log = LoggerFactory.getLogger("trends");
 
-    public static void main(String[] args) {
-        final String[] arguments = new String[]{"basic", ".", "."};
-        try {
-            final ArrayList<Term> terms = new TaxonomyExtractorBasic().getTerms(text);
-            final ArrayList<Topic> topics = new TaxonomyExtractorBasic().extract(terms);
+    private static String topicToString(final Topic topic) {
+        return String.format(
+                "[%d %s | %s]",
+                topic.id(), topic.name(), topic.getTermNames().toString());
+    }
 
-//            Extract.main(arguments);
+    public static void main(String[] args) {
+        try {
+            final TaxonomyExtractor extractor = new TaxonomyExtractorBasic();
+            final ArrayList<Term> terms = extractor.getTerms(text);
+            final ArrayList<Topic> topics = extractor.extract(terms);
+            for (Topic topic : topics) {
+                log.info(topicToString(topic));
+            }
         } catch (Exception e) {
             log.error("poo-tee-weet", e);
         }
     }
 
-    private final static String text = "One man's nocturnal emission into the pulsating streets of Paris. Drinks will be had, porn will be stolen, and balls will be played with in this night of electronic debauchery. Click through for the video with a very NSFW splash screen.\n" +
-
-            "Turn Out the Lights is the latest single and title track from Tom Dinsdale's (formerly of Audio Bully) new project, Mineo. P.E. Joubert directed this, the official music video. You can listen to the full song on Soundcloud and get the album from Beatport.\n";
+    private final static String text = "One man's nocturnal emission into " +
+            "the pulsating streets of Paris. Drinks will be had, porn will be " +
+            "stolen, and balls will be played with in this night of electronic " +
+            "debauchery. Click through for the video with a very NSFW splash screen. " +
+            "Turn Out the Lights is the latest single and title track from Tom " +
+            "Dinsdale's (formerly of Audio Bully) new project, Mineo. P.E. Joubert " +
+            "directed this, the official music video. You can listen to the full " +
+            "song on Soundcloud and get the album from Beatport.\n";
 }
