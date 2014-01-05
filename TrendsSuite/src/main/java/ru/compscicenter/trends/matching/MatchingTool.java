3d15c54faf5e303622503a6274d46961995dd10a
alexeyev <anton.m.alexeyev@gmail.com>
2014-01-06 02:14:46 +0400

    Matching strategy for great recall.

--- TrendsSuite/src/main/java/ru/compscicenter/trends/matching/MatchingTool.java
+++ TrendsSuite/src/main/java/ru/compscicenter/trends/matching/MatchingTool.java
@@ -1,9 +1,12 @@
 package ru.compscicenter.trends.matching;
 
+import org.apache.commons.io.FileUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.File;
+import java.util.Collection;
+import java.util.List;
 
 /**
  * For experiments.
@@ -16,9 +19,25 @@ public class MatchingTool {
     public static void main(String[] args) {
         try {
             LuceneMatcher matcher = new LuceneMatcher(new File("techcrunch.txt"));
-            log.info(matcher.fuzzySearch("Shmoogle", 4).toString());
-            log.info(matcher.magicSearch("mail.ru", 7f).toString());
-            log.info(matcher.magicSearch("mail.ru group", 6f).toString());
+            List<String> lines = FileUtils.readLines(new File("test_companies.tsv"));
+            for (String line : lines) {
+                String query = line.replaceAll("[/\"\\)\\(\\*\\?\\]\\[\\s\\+]+", " ").replaceFirst("NOT", "");
+                log.warn(query);
+                Collection<String> results = matcher.magicSearch(query, 3f);
+                log.info(results.toString());
+                if (results.isEmpty()) {
+                    String[] shortQuery = query.split("\\s");
+                    int len = shortQuery.length;
+                    int i = 0;
+                    while (results.isEmpty() && i < len) {
+                        if (!shortQuery[i].equals("")) {
+                            results = matcher.magicSearch(shortQuery[i], 3f);
+                            log.info("==> " + results.toString());
+                        }
+                        i++;
+                    }
+                }
+            }
         } catch (Exception e) {
             log.error("Oops", e);
         }
